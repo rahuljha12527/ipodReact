9d662d8cb356c76561656d0e85041e373fe9d9f9
rahuljha661 <rahuljha12527@gmail.com>
2020-08-26 16:43:13 +0530

    Music js

---
+++ src/Screen.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class Screen extends Component {
+    render() {
+        return (
+            <div style={styles.screen} id='screen-container'>
+                {this.props.activePage==='Home'?<Home />}
+            </div>
+        );
+    }
+}
+
+const styles={
+    screen:{
+        height:'50%',
+        width:'95%',
+        borderRadius:'12px',
+        backgroundColor:'white',
+        border:'2px solid black',
+        marginTop:'1rem'
+    }
+}
+
+export default Screen;
